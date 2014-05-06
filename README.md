44fa3964b69ec2b6c838d32f8b5ec46ba0a9e763
Cristiano Betta <cbetta@paypal.com>
2014-05-06 16:27:17 +0200

    Cleaning up code
    * App now port independent
    * Updated to use single quotes as much as needed
    * Updated documentation to clarify that PHP 5.4 is only needed to use the build in PHP server

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 This is an example of the PayPal Express Checkout API using plain PHP to make a PayPal payment.
 
+This code does not use an SDK although it uses a basic wrapper to handle the PHP NVP api.
+
 ## Technology
 
 This demo uses
 
-* PHP 5.4 or higher
+* PHP
 
 ## Running the demo
 
-* Run `php -S 127.0.0.1:8080` to start the app
+* Run `php -S 127.0.0.1:8080` to start the app (requires PHP 5.4 or above) or load it in your web server of choice.
 * Visit `http://127.0.0.1:8080/` in your browser
 * You will be redirected to PayPal
 * Login using the following credentials:
--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
 
-require("includes/config.php");
-require("includes/paypal.php");
+require('includes/config.php');
+require('includes/paypal.php');
 
 $paypal = new PayPal($config);
 
 $result = $paypal->call(array(
-  "method" => "SetExpressCheckout",
-  "paymentrequest_0_paymentaction" => "sale",
-  "paymentrequest_0_amt" => "19.99",
-  "paymentrequest_0_currencycode" => "USD",
-  "returnurl" => "http://127.0.0.1:8080/success.php",
-  "cancelurl" => "http://127.0.0.1:8080/cancel.php",
+  'method' => 'SetExpressCheckout',
+  'paymentrequest_0_paymentaction' => 'sale',
+  'paymentrequest_0_amt' => '19.99',
+  'paymentrequest_0_currencycode' => 'USD',
+  'returnurl' => 'http://'.$_SERVER['HTTP_HOST'].'/success.php',
+  'cancelurl' => 'http://'.$_SERVER['HTTP_HOST'].'/cancel.php',
 ));
 
-if ($result["ACK"] == 'Success') {
+if ($result['ACK'] == 'Success') {
   $paypal->redirect($result);
 } else {
-  echo "Handle the payment creation failure";
+  echo 'Handle the payment creation failure';
 }
